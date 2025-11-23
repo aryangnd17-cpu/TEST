7a880b2338a166dc795e37fbfa9c4fb20b317cc3
aryangnd17-cpu <aryangnd17@gmail.com>
2025-11-24 00:02:42 +0530

    Update bank_acc.py

--- bank_acc.py
+++ bank_acc.py
@@ -70,30 +70,7 @@ class savingsAcc(InterestRewardAcc):
             print(f"Withdraw interupted: {error}")  
 
 
-from bank_acc import *
-
-Aryan = Bank_Account(10000, "Aryan")
-Vipul = Bank_Account(12000, "Vipul")
-
-Aryan.get_balance()
-Vipul.get_balance()
-
-
-Vipul.deposite(5000)
-Vipul.withdraw(3000)
-Vipul.tranfer(10000,Aryan)
-
-Manish = InterestRewardAcc(20000, "Manish")
-Manish.deposite(5000)
-Manish.get_balance()
-Manish.tranfer(3000,Aryan)
-
-Vivek = savingsAcc(15000, "Vivek")
-
-Vivek.get_balance()
-Vivek.deposite(4000)
-Vivek.tranfer(70000, Vipul)
-Vivek.tranfer(7000, Vipul)
                         
 
     
+
